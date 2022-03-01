eaa2be72c1513fac492829ee8c5886567319b0cf
sneakstarberry <gihong2012@likelion.org>
2022-03-01 18:37:57 +0900

    fix: index.d.ts 위치

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export default UnityView;
+import { UnityModule } from "./types/src/UnityModule";
+import MessageHandler from "./types/src/MessageHandler";
+import { UnityViewMessage } from "./types/src/UnityModule";
+export { UnityModule, MessageHandler, UnityViewMessage, UnityResponderView };
--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export default UnityView;
-import { UnityModule } from "./src/UnityModule";
-import MessageHandler from "./src/MessageHandler";
-import { UnityViewMessage } from "./src/UnityModule";
-export { UnityModule, MessageHandler, UnityViewMessage, UnityResponderView };
